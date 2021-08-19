e6397f35b71dfb371d96ee07f09c547aba105fe0
root <root@ip-10-0-3-170.ec2.internal>
2021-08-19 09:57:30 +0000

    file added

---
+++ filedate.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /tmp
+today_Date=`date +%d`
+today_month=`date +%b`
+file_date=`ls -l file.txt|tr -s ' ' |cut -d ' ' -f7`
+file_month=`ls -l file.txt|tr -s ' ' |cut -d ' ' -f6`
+if [ $today_Date -eq $file_date ] && [ $today_month == $file_month ]
+then
+echo "file available for today's date"
+else 
+echo "file is not for today"
+fi
