c4d466465d5ce6dc31fa4f16cb55bb9f5b6b065b
root <root@ip-10-0-3-144.ec2.internal>
2021-09-11 11:34:06 +0000

    spaceissue

---
+++ spaceissue.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+threshold_value=0
+run_folder_space=`df -hk | grep run |head -1|tr -s ' '|cut -d ' ' -f5|cut -c1`
+echo $run_folder_space
+if [ $run_folder_space -gt $threshold_value ]
+then 
+	echo "size is more that the threshold_value , please take action immediately"
+else
+	echo "all file"
+fi
